d136c6e3004de9241a47adf676fc6c427f843e4a
maugarciav <mau.villanueva2002@gmail.com>
2024-01-24 17:58:43 -0600

    199. Binary Tree Right Side View

---
+++ 199. Binary Tree Right Side View/199.js
@@ -0,0 +1,33 @@
+// Description: https://leetcode.com/problems/binary-tree-right-side-view/description/?envType=study-plan-v2&envId=leetcode-75
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+  if(!root) return []
+
+  const queue = [root]
+  const result = []
+
+  while(queue.length){
+      let len = queue.length
+      result.push(queue[queue.length -1].val)
+
+      while(len--){
+          let node = queue.shift()
+          if(node.left) queue.push(node.left)
+          if(node.right) queue.push(node.right)
+      }
+  }
+  
+  return result
+};
