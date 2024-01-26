807b0e9c8a12b5a78857ba0366ce96f92d84c63b
maugarciav <mau.villanueva2002@gmail.com>
2024-01-26 17:24:57 -0600

    1161. Maximum Level Sum of a Binary Tree -> format

--- 1161. Maximum Level Sum of a Binary Tree/1161.js
+++ 1161. Maximum Level Sum of a Binary Tree/1161.js
@@ -12,29 +12,28 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxLevelSum = function(root) {
+var maxLevelSum = function (root) {
   const queue = [root]
   let res = root.val
   let lvl = 1
   let final = lvl
 
-  while(queue.length){
-      let len = queue.length
-      let tres = 0
+  while (queue.length) {
+    let len = queue.length
+    let tres = 0
 
-
-      while(len--){
-          let node = queue.shift()
-          tres += node.val
-          if(node.left) queue.push(node.left)
-          if(node.right) queue.push(node.right)
-      }
-      if(tres > res){
-          res = tres
-          final = lvl
-      }
-      lvl++
+    while (len--) {
+      let node = queue.shift()
+      tres += node.val
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+    if (tres > res) {
+      res = tres
+      final = lvl
+    }
+    lvl++
   }
-  
+
   return final
-};
+}
